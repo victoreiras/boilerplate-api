a28dccabea5989f83b6eceacb10b661f30e6d296
Victor Eiras <victor.eiras@vertem.com>
2025-03-31 09:06:10 -0300

    Complementando cenários de teste para CreateProject

--- Boilerplate/test/Boilerplate.Test.Unit/Application/Usecases/CreateProjectTests.cs
+++ Boilerplate/test/Boilerplate.Test.Unit/Application/Usecases/CreateProjectTests.cs
@@ -4,12 +4,13 @@ using Boilerplate.Domain.Entities;
 using Boilerplate.Domain.Repositories;
 using Boilerplate.Infrastructure.Repositories;
 using Moq;
+using FluentAssertions;
 
 namespace Boilerplate.Test.Unit.Application.Usecases;
 
 public class CreateProjectTests
 {
-    [Fact(DisplayName = "Should create project")]
+    [Fact(DisplayName = "Should create a project")]
     public async Task Should_Create_Project()
     {      
         var input = new ProjectDto(
@@ -31,5 +32,24 @@ public class CreateProjectTests
         Assert.Equal(result.Description, input.Description);
         Assert.Equal(result.BeginDate, DateOnly.FromDateTime(DateTime.UtcNow));
         Assert.Equal(result.EndDate, DateOnly.FromDateTime(DateTime.UtcNow.AddDays(10)));
+    }
+
+    [Fact(DisplayName = "Should not create a project with end date less than today")]
+    public async Task ShouldNotCreateAProjectWithoutEndDate()
+    {
+        var projectRepository = new ProjectRepository();
+        var createProject = new CreateProject(projectRepository);
+
+        var input = new ProjectDto(
+            Name: "Nome do Projeto",
+            Description: "Descrição do Projeto",
+            EndDate: DateOnly.FromDateTime(DateTime.Now.AddDays(-1))
+        );
+
+        var result = await createProject.Execute(input);
+
+        result.IsSuccess.Should().Be(false);
+        result.Data.Should().BeNull();
+        result.Messages.Should().Be("the end date cannot be less than today's date");
     }
 }
